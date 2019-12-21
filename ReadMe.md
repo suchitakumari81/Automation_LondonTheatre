1822cba0ff545abbff881f6d525c9e4a5c13609a
Suchita <suchkuma@cisco.com>
2019-12-21 23:08:44 +0530

    added report

--- ReadMe.md
+++ ReadMe.md
@@ -1,7 +1,20 @@
 Installing Packages
 
-Python :3.7
-Selenium:3.4
-Configparser
+Python : 3.7
+Selenium: 3.4
+Configparser 
+HtmlTestRunner For Reporting
+
+
+Running Test :
+
+Run from Pycharm : TestFactory/allsuite.py
+
+
+
+
+
+
+
 
 
--- TestFactory/allsuite.py
+++ TestFactory/allsuite.py
@@ -6,15 +6,6 @@ from HtmlTestRunner import HTMLTestRunner
 
 import TestFactory.TestLondanTheatre
 
-folder = './Snapshots/Chrome/ActualScreenshots'
-for the_file in os.listdir(folder):
-    file_path = os.path.join(folder, the_file)
-    try:
-        if os.path.isfile(file_path):
-            os.unlink(file_path)
-        # elif os.path.isdir(file_path): shutil.rmtree(file_path)
-    except Exception as e:
-        print(e)
 
 suite = unittest.TestSuite()
 
